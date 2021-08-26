3b65b401ad05f6396f0252c20d9343b3b9b0906e
MuneneSapiens <wanderi.mwangi8@gmail.com>
2021-08-26 20:07:04 +0300

    dbservice working

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express=require(`express`);
-const app=express();
 const cors= require(`cors`);
 const dotenv=require(`dotenv`);
-const { urlencoded } = require("express");
+var app=express();
+//const { urlencoded, response } = require("express");
 dotenv.config();
+const dbService =require(`./dbservice`);
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+ 
 
 
-app.get('/',(req,res)=>{
-    console.log('Home page');
-    res.send('Home page');
-});
+app.get('/', function (req, res) {
+    res.send('hello world');
+  });
 //create
 app.post('/insert',(req,res)=>{
 
 });
 //read
-app.get('/getAll',(req,res)=>{
-    console.log('test');
+app.get('/getAll',function(req,res){
+    console.log("test");
+  
+
 });
 
 //update
